1b7c919dd4bb1e62c31049189325b09483918c35
petros94 <petros.stergioulas94@gmail.com>
2020-10-08 11:23:39 +0200

    Zipkin

--- src/main/java/dev/petros0/monitoring/MonitoringApplication.java
+++ src/main/java/dev/petros0/monitoring/MonitoringApplication.java
@@ -1,13 +1,27 @@
 package dev.petros0.monitoring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Slf4j
 public class MonitoringApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(MonitoringApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(MonitoringApplication.class, args);
+  }
 
+  @RestController
+  class DefaultController {
+
+    @GetMapping("/sysout")
+    public String sysout() {
+      log.info("Hello World!!!");
+
+      return "Hello World!";
+    }
+  }
 }
